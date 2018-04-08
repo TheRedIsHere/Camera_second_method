5f432e7179b179604642cd2aea0a06a76e7bdf7b
TheRedIsHere <mitya.gorbachev.2013@mail.ru>
2018-04-08 19:59:24 +0300

    camera

---
+++ app/src/main/java/by/bstu/pnv/education/camera_/MainActivity.java
@@ -0,0 +1,108 @@
+package by.bstu.pnv.education.camera_;
+
+import java.io.IOException;
+
+import android.app.Activity;
+import android.graphics.Matrix;
+import android.graphics.RectF;
+import android.hardware.Camera;
+import android.hardware.Camera.CameraInfo;
+import android.hardware.Camera.Size;
+import android.os.Bundle;
+import android.view.Display;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends Activity {
+
+    SurfaceView sv;
+    SurfaceHolder holder;
+    HolderCallback holderCallback;
+    Camera camera;
+
+    final int CAMERA_ID = 0;
+    final boolean FULL_SCREEN = true;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        sv = (SurfaceView) findViewById(R.id.surfaceView);
+        holder = sv.getHolder();
+        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+
+        holderCallback = new HolderCallback();
+        holder.addCallback(holderCallback);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        camera = Camera.open(CAMERA_ID);
+        setPreviewSize(FULL_SCREEN);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (camera != null)
+            camera.release();
+        camera = null;
+    }
+
+    class HolderCallback implements SurfaceHolder.Callback {
+
+        @Override
+        public void surfaceCreated(SurfaceHolder holder) {
+            try {
+                camera.setPreviewDisplay(holder);
+                camera.startPreview();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void surfaceChanged(SurfaceHolder holder, int format, int width,
+                                   int height) {
+            camera.stopPreview();
+            setCameraDisplayOrientation(CAMERA_ID);
+            try {
+                camera.setPreviewDisplay(holder);
+                camera.startPreview();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void surfaceDestroyed(SurfaceHolder holder) {
+
+        }
+
+    }
+
+    void setPreviewSize(boolean fullScreen) {
+        sv.getLayoutParams().height = 1600;
+        sv.getLayoutParams().width = 1000;
+    }
+
+    void setCameraDisplayOrientation(int cameraId) {
+        int result = 0;
+
+        CameraInfo info = new CameraInfo();
+        Camera.getCameraInfo(cameraId, info);
+        if (info.facing == CameraInfo.CAMERA_FACING_BACK) {
+            result = (360 + info.orientation);
+        }
+        result = result % 360;
+        camera.setDisplayOrientation(result);
+    }
+}
